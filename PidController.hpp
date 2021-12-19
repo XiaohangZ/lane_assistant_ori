1d1355543a4c3c2ed595c8967a9c2e20dd9eb8ce
XiaohangZ <92926691+XiaohangZ@users.noreply.github.com>
2021-12-19 22:08:30 +0100

    Create PidController.hpp

---
+++ PidController.hpp
@@ -0,0 +1,49 @@
+class PidController
+{
+public:
+    PidController( double Kp_, double Ki_, double Kd_ ) : Kp( Kp_ ), Ki( Ki_ ), Kd( Kd_ )
+    {
+        p_error = 0.;
+        i_error = 0.;
+        d_error = 0.;
+    }
+
+    /**
+     * Update each term in the PID error variables given the current error
+     * @param error The current error
+     */
+    inline void UpdateErrorTerms( double error )
+    {
+        d_error = error - p_error;
+        i_error += error;
+        p_error = error;
+    }
+
+    /**
+     * Calculate the each term of the PID error
+     * @param printValue print the error and result in pid controller
+     * @output the total command to the actuator
+     */
+    double OutputToActuator( double d_limit, bool printValue );
+
+	/*set the i_error in pid controller to 0*/
+    inline void setZero()
+    {
+        i_error = p_error = 0;
+    }
+
+private:
+    /**
+     * PID Error terms
+     */
+    double p_error;
+    double i_error;
+    double d_error;
+
+    /**
+     * PID Gain coefficients
+     */
+    double Kp;
+    double Ki;
+    double Kd;
+};
